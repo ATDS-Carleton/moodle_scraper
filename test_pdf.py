927c518dcc32032ca40909e19f473394afa03b55
xxf1995 <xiax@carleton.edu>
2016-12-18 04:38:05 -0600

    adjust params to display fully

--- test_pdf.py
+++ test_pdf.py
@@ -3,7 +3,7 @@ def execute(script, args):
     driver.execute('executePhantomScript', {'script': script, 'args' : args })
 
 driver = webdriver.PhantomJS('phantomjs')
-
+driver.set_window_size(1920,1200)
 # hack while the python interface lags
 driver.command_executor._commands['executePhantomScript'] = ('POST', '/session/$sessionId/phantom/execute')
 
@@ -11,7 +11,7 @@ driver.get('http://xxf1995.github.io/')
 
 # set page format
 # inside the execution script, webpage is "this"
-pageFormat = '''this.paperSize = {format: "A4", orientation: "portrait" };'''
+pageFormat = '''this.paperSize = {format: "A2", orientation: "portrait" };'''
 execute(pageFormat, [])
 
 # render current page
